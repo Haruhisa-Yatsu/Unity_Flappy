3d63ef122533f13ddd8fe51f7921ca9ab83a4fe1
HaruhisaYatsu <yatsu.haruhisa@nsg.gr.jp>
2024-05-16 11:25:07 +0900

    タイトル追加　壁の生成位置修正　壁の自動生成処理

--- Assets/Scripts/WallGenerator.cs
+++ Assets/Scripts/WallGenerator.cs
@@ -6,6 +6,20 @@ public class WallGenerator : MonoBehaviour
 {
     public Wall _wallPrefab;
 
+    /// <summary>
+    /// 生成感覚のカウンタ
+    /// </summary>
+    private float _generateCounter;
+
+    /// <summary>
+    /// 壁の生成間隔（秒）
+    /// </summary>
+    public float _generateSecond = 1.0f;
+
+    /// <summary>
+    /// 壁オブジェクト生成時に上下にずらす最大値
+    /// </summary>
+    public float _maxShift = 100.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,10 +30,19 @@ public class WallGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // １秒で　_generateCounterが大体１増える
+        _generateCounter += Time.deltaTime;
+
         // キーボードの[P]を入力したとき
-        if(Input.GetKeyDown(KeyCode.P))
+        if(_generateCounter > _generateSecond)
         {
-            Instantiate(_wallPrefab,transform);
+            _generateCounter -= _generateSecond;
+            var wall = Instantiate(_wallPrefab,transform);
+
+            var pos = wall.transform.localPosition;
+            pos.y += Random.Range(-1.0f ,1.0f) * _maxShift;
+            wall.transform.localPosition = pos;
+
         }
     }
 }
